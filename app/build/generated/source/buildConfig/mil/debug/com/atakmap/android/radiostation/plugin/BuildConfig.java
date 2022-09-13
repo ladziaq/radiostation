511fb1e440cd472884653c4d0302a8523c13ea81
marcin.ladziak@student.wat.edu.pl <SoundBox1>
2022-09-13 15:02:07 +0200

    nowe

---
+++ app/build/generated/source/buildConfig/mil/debug/com/atakmap/android/radiostation/plugin/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.atakmap.android.radiostation.plugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.atakmap.android.radiostation.plugin";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "mil";
+  public static final int VERSION_CODE = 1662969094;
+  public static final String VERSION_NAME = "4.5.1 (971b0a5f) - [4.5.1]";
+  // Field from product flavor: mil
+  public static final String ATAK_PACKAGE_NAME = "com.atakmap.app.civ";
+}
